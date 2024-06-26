e0e7349d31d1fcd1498106471906fb1b8164bceb
Wincent Balin <wincent.balin@gmail.com>
2024-06-26 19:06:53 +0200

    Simplified handling of GET parameters
    by using URLSearchParams class.

--- script.js
+++ script.js
@@ -5,27 +5,24 @@ document.querySelector('#print').addEventListener('click', function(event) {
 var inputIds = ['backaddress', 'addressnote', 'address', 'additionalinformation', 'text', 'footer'];
 
 document.querySelector('#share').addEventListener('click', function(event) {
-    var params = inputIds.filter(function(id) {
+    var params = new URLSearchParams();
+    inputIds.forEach(function(id) {
         var text = document.getElementById(id).innerText;
-        return text.trim() !== '';
-    }).map(function(id) {
-        var text = document.getElementById(id).innerText;
-        return [id, '=', encodeURIComponent(text)].join('');
-    }).join('&');
+        if (text.trim() !== '') {
+            params.set(id, encodeURIComponent(text));
+        }
+    });
     var url = [document.location.origin, document.location.pathname,
-               '?', params].join('');
+               '?', params.toString()].join('');
     window.open(url, '_blank');
 });
 
 if (document.location.search) {
-    document.location.search.substring(1).split('&').map(function(param) {
-        return param.split('=');
-    }).filter(function(kv) {
-        var id = kv[0];
-        return inputIds.indexOf(id) !== -1;
-    }).forEach(function(kv) {
-        var id = kv[0];
-        var text = decodeURIComponent(kv[1]);
-        document.getElementById(id).innerText = text;
+    var params = new URLSearchParams(document.location.search);
+    inputIds.forEach(function(id) {
+        if (params.has(id)) {
+            var text = decodeURIComponent(params.get(id));
+            document.getElementById(id).innerText = text;
+        }
     });
 }
